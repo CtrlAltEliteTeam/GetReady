fc628daaad403640b7571790c21c552f1751ddb1
Gabey01 <101514971+Gabey01@users.noreply.github.com>
2022-09-05 23:29:43 +0200

    Add files via upload

---
+++ client/src/components/landing/LandingPage.test.js
@@ -0,0 +1,9 @@
+import {LandingPage} from "./LandingPage";
+import {render} from "@testing-library/react";
+
+describe(LandingPage, () => {
+    it("h1 is landing page", () => {
+    
+        expect(5).toEqual(5);
+    });
+});
