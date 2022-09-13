0b05c354f10e098532aaf12004ec371f4bdddc5f
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2022-09-13 11:14:10 +0200

    moved landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import LandingPage from '../src/components/landing/LandingPage';
+import LandingPage from '../src/pages/landing/LandingPage';
 import LoginPage from '../src/components/login/LoginPage';
 import Signup from '../src/components/login/Signup';
 import Dashboard from '../src/pages/dashboard/Dashboard';
