d47119601d2acfb33247b547a9d58a0695a7a677
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2022-09-18 19:21:45 +0200

    crude create game form needs css

--- client/src/components/createTounament/CreateTournament.js
+++ client/src/components/createTounament/CreateTournament.js
@@ -5,24 +5,81 @@ import Select from "react-dropdown-select";
 const CreateTournament = () => {
 
     const [tName, setTName] = useState('');
-    const [tGame, setTGame] = useState([]);
+    const [tGame, setTGame] = useState("");
+    const [tsDate, setTsDate] = useState("");
+    const [tfDate, setTfDate] = useState("");
+    const [tParticipants, setTParticipants] = useState(2);
+    const [tDesc, setTDesc] = useState("");
+    const [tsTime, setTsTime] = useState("");
 
-    let options = [{id:1,name:"Counter Strike: GO"}]//,"Fortnite","Halo","League of Legends","Super Smash Bros"];
+    const options = [{id:1,name:"Counter Strike: GO"},{id:2,name:"Fortnite"}]//,"Fortnite","Halo","League of Legends","Super Smash Bros"];
 
+    useEffect(() => {
+        console.log(tGame);
+    }, [tGame])
+
+    const handleSubmit = async (e) => {
+        e.preventDeafault();
+    }
+    
     return(
-        <div className="create-tounament-outer">
-            <form>
+        <section className="create-tounament-outer">
+            <form encType='multipart/form-data' onSubmit={handleSubmit}>
                 <input
-                    type="tname"
+                    type="name"
                     id="tname"
                     placeholder="Tounament Name"
                     value={tName}
                     onChange={(e) => setTName(e.target.value)}
                     required
                 />
-                <Select options={options} onChange={(values) => this.setTGame(values)} />
+                <select
+                    value={tGame}
+                    onChange={(e) => setTGame(e.target.value)}
+                >
+                    {options.map(({ id, name }, index) => <option value={id} >{name}</option>)}
+                </select>
+                <input
+                    type="date"
+                    id="tsdate"
+                    value={tsDate}
+                    onChange={(e) => setTsDate(e.target.value)}
+                    required
+                    placeholder="Tounament Name"
+                />
+                <input
+                    type="time"
+                    id="tstime"
+                    value={tsTime}
+                    required
+                    onChange={(e) => setTsTime(e.target.value)}
+                />
+                <input
+                    type="date"
+                    id="tfdate"
+                    value={tfDate}
+                    required
+                    onChange={(e) => setTfDate(e.target.value)}
+                />
+                <input
+                    type="number"
+                    id="tstime"
+                    value={tParticipants}
+                    required
+                    onChange={(e) => setTParticipants(e.target.value)}
+                />
+                <textarea
+                    type="time"
+                    id="tstime"
+                    value={tDesc}
+                    onChange={(e) => setTDesc(e.target.value)}
+                    required
+                />
+                <button>
+
+                </button>
             </form>
-        </div>
+        </section>
     )
 }
 
