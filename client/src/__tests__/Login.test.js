028ab0c7a98e44436e98fd1279db6a9c1489b000
Sesh-wits <2081236@students.wits.ac.za>
2022-10-04 20:09:05 +0200

    register and login returns user_id and username

--- client/src/__tests__/Login.test.js
+++ client/src/__tests__/Login.test.js
@@ -1,9 +1,10 @@
 import React, { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Login from '../components/login/Login';
+import { AuthProvider } from '../api/AuthProvider';
 
 test('render header', () =>{
-    render(<Login />);
+    render(<AuthProvider value = "0" > <Login /> </AuthProvider>);
     const getHeadingElement = screen.getByTestId("test");
     expect(getHeadingElement).toBeInTheDocument();
 });
