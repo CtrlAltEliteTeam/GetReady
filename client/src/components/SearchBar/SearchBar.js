901fbd6c280f19e824df723f33eca4c0ad31a034
Sesh-wits <2081236@students.wits.ac.za>
2022-10-08 23:13:04 +0200

    search function working - may need to be formatted

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./SearchBar.css";
-import SearchIcon from "@material-ui/icons/Search";
-import CloseIcon from "@material-ui/icons/Close";
+import * as FcIcons from 'react-icons/fc';
+import * as IoIcons from 'react-icons/io';
+import GameTile from "../../components/GameTile/GameTile";
 
 function SearchBar({placeholder, data}) {
+    const [filteredData, setFilteredData] = useState([]);
+    const [barValue, setBarValue] = useState("");
 
+    const handleFilter = (event) => {
+        const searchString = event.target.value;
+        setBarValue(searchString);
+        const newFilter = data.filter((value) => {
+            return value.game.toLowerCase().includes(searchString.toLowerCase());
+        });
+        if (searchString === ""){
+            setFilteredData([]);
+        } else {
+            setFilteredData(newFilter);
+        }
+    };
 
+    const clearInput = () => {
+        setBarValue("");
+        setFilteredData([]);
+    }
 
     return (
         <div className="search">
             <div className="searchInput"> 
             <input 
                 type="text" 
-                placeholder={placeholder}
+                placeholder={placeholder} value={barValue} onChange={handleFilter}
             /> 
             <div className="searchIcon">
-                <SearchIcon />
+                {barValue.length == 0 ? <FcIcons.FcSearch/> : <IoIcons.IoMdCloseCircleOutline id="clearBtn" onClick={clearInput}/>}
             </div>
         </div>
-        <div className="dataResult">  </div>
+        {filteredData.length != 0 &&(
+        <div className="dataResult">
+            {filteredData.map((element) => {
+                return (
+                    <div key={element.id}> 
+                        <GameTile game={element}/>
+                    </div>
+                )
+            })}
+        </div>
+        )}
         </div>
 
     );
