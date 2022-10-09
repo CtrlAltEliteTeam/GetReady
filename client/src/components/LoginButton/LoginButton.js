dc372e9bba8e945fe4a5e335c7cf56a40dfe51e5
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2022-10-09 12:35:28 +0200

    signup button

--- client/src/components/LoginButton/LoginButton.js
+++ client/src/components/LoginButton/LoginButton.js
@@ -15,32 +15,22 @@ const LoginButton = () => {
 
     const [showSignupButton, setShowSignupButton] = useState(true);
 
+    const [showLoginButtons, setShowLoginButtons] = useState(true);
+
+    //this needs its own var
     const {pathname} = useLocation();
     //console.log(pathname);
     useEffect(() => {
         const match = matchPath("/login/*",pathname);
         if (match != null) {
-            setShowLoginButton(false);
-            setShowSignupButton(false);
+            setShowLoginButtons(false);
         } else {
-            setShowLoginButton(true);
-            setShowSignupButton(true);
+            setShowLoginButtons(true);
         }
     });
 
     useEffect(() => {
         console.log(auth.user_id);
-            if(auth.user_id != 0){
-                setLoginName('Log Out');
-                setShowSignupButton(false);
-            }
-            else {
-                setLoginName('Login');
-                setShowSignupButton(true);
-            }
-    },[auth.user_id]);
-
-    useEffect(() => {
             if(auth.user_id != 0){
                 setLoginName('Log Out');
                 setShowSignupButton(false);
@@ -66,11 +56,13 @@ const LoginButton = () => {
 
     return (
         <>
-            <div className={showLoginButton ? "login-button-button-show" : "login-button-button"}>
-                <div  className="login-button-button-button" onClick={loginCheck}>{loginName}</div>
-            </div>
-            <div className={showSignupButton ? "signup-button-button-show" : "signup-button-button"}>
-                <div  className="signup-button-button-button" onClick={signupClick}>Signup</div>
+            <div className={showLoginButtons ? 'login-button-button-container-show' : 'login-button-button-container'}>
+                <div className={showLoginButton ? "login-button-button-show" : "login-button-button"}>
+                    <div  className="login-button-button-button" onClick={loginCheck}>{loginName}</div>
+                </div>
+                <div className={showSignupButton ? "signup-button-button-show" : "signup-button-button"}>
+                    <div  className="signup-button-button-button" onClick={signupClick}>Signup</div>
+                </div>
             </div>
         </>
     )
