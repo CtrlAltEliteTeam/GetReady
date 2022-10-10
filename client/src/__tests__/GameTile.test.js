efda6fb468e018ca8651f88998e5ecd8cd5fc9b3
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2022-10-10 14:17:42 +0200

    object Test

--- client/src/__tests__/GameTile.test.js
+++ client/src/__tests__/GameTile.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen, act, fireEvent, waitFor } from "@testing-library/react";
 import '@testing-library/jest-dom';
 import GameTile from '../components/GameTile/GameTile';
+import { GameTileData } from '../components/GameTile/GameTileData';
 import ReactDOM from 'react-dom/client';
 import ReactTestUtils from 'react-dom/test-utils';
 
@@ -10,7 +11,6 @@ import ReactTestUtils from 'react-dom/test-utils';
 const params1 = {user_id: 0, game:{id : 1, name: 'fortnite tourny', game: 'Fortnite', img: "Fortnite" , alt : "TournamentImage", content : "TOURNAMENT", user: 2, count : 1 }, editPermission : false}
 const params2 = {user_id: 0, game:{id : 1, name: 'fortnite tourny', game: 'Fortnite', img: "Fortnite" , alt : "TournamentImage", content : "GAME", user: 2, count : 1 },  editPermission : false}
 const params3 = {user_id: 2, game:{id : 1, name: 'fortnite tourny', game: 'Fortnite', img: "Fortnite" , alt : "TournamentImage", content : "TOURNAMENT", user: 2, count : 1 },  editPermission : true}
-
 let container;
 
 beforeEach(() => {
@@ -102,4 +102,12 @@ describe('Game Tile tests',() => {
         expect(container.querySelector('div.tournament-overlay')).toBeVisible();
 
     });
-})
+});
+
+describe('Game Tile data tests',() => {
+    it('Object Correct',() => {
+
+        const tile = new GameTileData(1,'fortnite tourny', 'Fortnite', "TOURNAMENT" ,1)
+
+    });
+});
